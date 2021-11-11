40d5ef2a11b945700f55f7ed3bc649b5cce02bbb
hanhbyte <hell.boyx229@gmail.com>
2021-11-11 14:30:54 +0700

    develop

---
+++ src/test/java/com/example/wschatapplication/WsChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wschatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsChatApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
